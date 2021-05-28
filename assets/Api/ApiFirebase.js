654dbede3145d0823891486694ec7e7346265a6c
rizaru-desu <rizaru_desu@yahoo.com>
2021-05-28 13:25:55 +0700

    Uplaod project FM

---
+++ assets/Api/ApiFirebase.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+export const firebaseConfig = {
+  apiKey: "AIzaSyAt5VILm_GN6Ak9uWq1BHQdZm_t4G877PY",
+  authDomain: "materi-farmasi.firebaseapp.com",
+  databaseURL: "https://materi-farmasi-default-rtdb.firebaseio.com",
+  projectId: "materi-farmasi",
+  storageBucket: "materi-farmasi.appspot.com",
+  messagingSenderId: "230163465589",
+  appId: "1:230163465589:web:3e3ccbb32fff9d47e87f3c",
+  measurementId: "G-2QQ2QPCJCD",
+};
